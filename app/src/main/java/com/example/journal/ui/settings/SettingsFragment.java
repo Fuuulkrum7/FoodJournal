93006d2944c3a51bac0e8e367e74d897885391d1
Fuuulkrum7 <ilka747428@gmail.com>
2022-03-21 14:51:44 +0500

    trying to create notifications

--- app/src/main/java/com/example/journal/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/journal/ui/settings/SettingsFragment.java
@@ -12,6 +12,8 @@ import android.widget.LinearLayout;
 import android.widget.Switch;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -22,13 +24,13 @@ public class SettingsFragment extends Fragment {
     // это будет именем файла настроек
     public static final String APP_PREFERENCES = "journal";
     public static final String APP_PREFERENCES_REMINDER = "Reminder";
+    public static final String[] APP_PREFERENCES_TIMES = new String[]{"breakfast_", "lunch_", "dinner_"};
+
     // Надо ли включать напоминалку
     Switch need_to_remind;
     // Получаем доступ к сохраненным данным в настройках
     SharedPreferences settings;
-    FoodTimer breakfast;
-    FoodTimer lunch;
-    FoodTimer dinner;
+    FoodTimer[] times = new FoodTimer[3];
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
@@ -40,14 +42,19 @@ public class SettingsFragment extends Fragment {
         // Получаем переключатель
         need_to_remind = (Switch) view.findViewById(R.id.Reminder);
 
-        // Создаем напоминалки (визуальные) и добавляем их
-        breakfast = FoodTimer.newInstance(7, 0, false);
-        lunch = FoodTimer.newInstance(14, 0, false);
-        dinner = FoodTimer.newInstance(18, 0, false);
+        for (int i = 0; i < APP_PREFERENCES_TIMES.length; i++){
+            // Создаем напоминалки (визуальные) и добавляем их
+            int hour = settings.getInt(APP_PREFERENCES_TIMES[i] + "hour", 8 + i * 6);
+            int minute = settings.getInt(APP_PREFERENCES_TIMES[i] + "minute", 0);
+            boolean isChecked = settings.getBoolean(APP_PREFERENCES_TIMES[i] + "checked", true);
+
+            times[i] = FoodTimer.newInstance(hour, minute, isChecked);
+        }
+
         FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-        ft.add(R.id.Timers, breakfast);
-        ft.add(R.id.Timers, lunch);
-        ft.add(R.id.Timers, dinner);
+        ft.add(R.id.Timers, times[0]);
+        ft.add(R.id.Timers, times[1]);
+        ft.add(R.id.Timers, times[2]);
         ft.commit();
 
         // Если в настройках сохранено предыдущая информация по уведомлениям
@@ -82,6 +89,33 @@ public class SettingsFragment extends Fragment {
         // Сохраняем все и вся
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean(APP_PREFERENCES_REMINDER, need_to_remind.isChecked());
+        int id = 101;
+
+        for (int i = 0; i < APP_PREFERENCES_TIMES.length; i++){
+            String time = APP_PREFERENCES_TIMES[i];
+            FoodTimer fragment = times[i];
+
+            editor.putBoolean(time + "checked", fragment.need_timer.isChecked());
+
+            if (fragment.need_timer.isChecked()){
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "Дневник питания")
+                        .setSmallIcon(R.drawable.notebook)
+                        .setContentTitle("Напоминание")
+                        .setContentText("Пора заполнить дневник")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(getActivity());
+                notificationManager.notify(id, builder.build());
+                id += 1;
+            }
+
+            String[] timer = fragment.time.getText().toString().split(":");
+            int hour = Integer.parseInt(timer[0]);
+            int minute = Integer.parseInt(timer[1]);
+
+            editor.putInt(time + "hour", hour);
+            editor.putInt(time + "minute", minute);
+        }
         editor.apply();
         super.onDestroyView();
     }

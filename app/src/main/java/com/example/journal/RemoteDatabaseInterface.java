cde12c5b6acfe843520528ed6128c1f402710442
System-IOException <56886020+IOExcept10n@users.noreply.github.com>
2022-05-31 22:32:20 +0500

    update of post rrequest

--- app/src/main/java/com/example/journal/RemoteDatabaseInterface.java
+++ app/src/main/java/com/example/journal/RemoteDatabaseInterface.java
@@ -3,6 +3,19 @@ package com.example.journal;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,40 +30,56 @@ public class RemoteDatabaseInterface {
 
     public void addUser(String login, String password, String name) {
         Log.d(MainActivity.TAG, "0");
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(LINK)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        JournalAPI journalAPI = retrofit.create(JournalAPI.class);
-
-        User user = new User(login, password, name, "");
-        Call<User> call = journalAPI.addUser(user);
-
-        call.enqueue(new Callback<User>() {
+        Thread thread = new Thread(){
             @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                if (!response.isSuccessful()){
-                    Log.d(MainActivity.TAG, "Server error: " + response.code() + "\n" + response.toString());
-                    return;
+            public void run() {
+                try {
+                    URL requestUrl = new URL(LINK + "/add_user.php");
+                    HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+                    connection.setDoOutput(true);
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Accept-Charset", "UTF-8");
+                    connection.setReadTimeout(10000);
+                    connection.setConnectTimeout(15000);
+                    try
+                    {
+                        connection.connect();
+                        DataOutputStream stream = new DataOutputStream(connection.getOutputStream());
+                        String json = String.format(
+                                "{ \"%s\" : \"%s\", \"%s\" : \"%s\", \"%s\" : \"%s\"}",
+                                USERNAME_FIELD, name, LOGIN_FIELD, login, PASSWORD_FIELD, password);
+                        Log.d(MainActivity.TAG, json);
+                        stream.writeBytes(json);
+                        stream.flush();
+                        stream.close();
+                        InputStream in = new BufferedInputStream(connection.getInputStream());
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        StringBuilder result = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            result.append(line);
+                        }
+                        int userID = 0;
+                        try
+                        {
+                            userID = Integer.parseInt(result.toString());
+                        }
+                        catch (NumberFormatException ex)
+                        {
+                            ex.printStackTrace();
+                            Log.e(MainActivity.TAG, "Bad server response.");
+                        }
+                        //Дальше, я полагаю, надо куда-то этот номер сохранить.
+                        Log.d("test", "result from server: " + result.toString());
+                    } finally {
+                        connection.disconnect();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(MainActivity.TAG, "Failure while creating a url or working with connection.");
                 }
-
-                User responseAPI = response.body();
-
-                Context context = MainActivity.getContext();
-                if (context == null){
-                    context = LoginFragment.getContext();
-                }
-
-                DatabaseInterface databaseInterface = new DatabaseInterface(context);
-                //Log.d(MainActivity.TAG, responseAPI.getId());
             }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                Log.d(MainActivity.TAG, "error");
-                t.printStackTrace();
-            }
-        });
+        };
+        thread.start();
     }
 }
